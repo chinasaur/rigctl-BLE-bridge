a7b63fc134803cdd156457e7d53961e32efb26f6
Peter K6PLI <k6pli@arrl.net>
2025-04-08 09:03:51 -0700

    Cleanup of bluez server example code.

---
+++ ble_server.py
@@ -0,0 +1,151 @@
+import array
+import sys
+import dbus
+import dbus.exceptions
+import dbus.service
+
+BLUEZ_SERVICE_NAME = 'org.bluez'
+GATT_MANAGER_IFACE = 'org.bluez.GattManager1'
+DBUS_OM_IFACE = 'org.freedesktop.DBus.ObjectManager'
+DBUS_PROP_IFACE = 'org.freedesktop.DBus.Properties'
+GATT_SERVICE_IFACE = 'org.bluez.GattService1'
+GATT_CHRC_IFACE = 'org.bluez.GattCharacteristic1'
+
+class Application(dbus.service.Object):
+    def __init__(self, bus):
+        self.path = '/'
+        self.services = []
+        dbus.service.Object.__init__(self, bus, self.path)
+
+    def get_path(self):
+        return dbus.ObjectPath(self.path)
+
+    def add_service(self, service):
+        self.services.append(service)
+
+    @dbus.service.method(DBUS_OM_IFACE, out_signature='a{oa{sa{sv}}}')
+    def GetManagedObjects(self):
+        response = {}
+        for service in self.services:
+            response[service.get_path()] = service.get_properties()
+            chrcs = service.get_characteristics()
+            for chrc in chrcs:
+                response[chrc.get_path()] = chrc.get_properties()
+        return response
+
+
+class Service(dbus.service.Object):
+    """
+    org.bluez.GattService1 interface implementation
+    """
+    PATH_BASE = '/org/bluez/example/service'
+
+    def __init__(self, bus, index, uuid, primary):
+        self.path = self.PATH_BASE + str(index)
+        self.bus = bus
+        self.uuid = uuid
+        self.primary = primary
+        self.characteristics = []
+        dbus.service.Object.__init__(self, bus, self.path)
+
+    def get_properties(self):
+        return {
+                GATT_SERVICE_IFACE: {
+                        'UUID': self.uuid,
+                        'Primary': self.primary,
+                        'Characteristics': dbus.Array(
+                                self.get_characteristic_paths(),
+                                signature='o')
+                }
+        }
+
+    def get_path(self):
+        return dbus.ObjectPath(self.path)
+
+    def add_characteristic(self, characteristic):
+        self.characteristics.append(characteristic)
+
+    def get_characteristic_paths(self):
+        result = []
+        for chrc in self.characteristics:
+            result.append(chrc.get_path())
+        return result
+
+    def get_characteristics(self):
+        return self.characteristics
+
+    @dbus.service.method(DBUS_PROP_IFACE,
+                         in_signature='s',
+                         out_signature='a{sv}')
+    def GetAll(self, interface):
+        if interface != GATT_SERVICE_IFACE:
+            raise InvalidArgsException()
+
+        return self.get_properties()[GATT_SERVICE_IFACE]
+
+
+class Characteristic(dbus.service.Object):
+    """org.bluez.GattCharacteristic1 interface implementation."""
+    def __init__(self, bus, index, uuid, flags, service):
+        self.path = service.path + '/char' + str(index)
+        self.bus = bus
+        self.uuid = uuid
+        self.service = service
+        self.flags = flags
+        self.descriptors = []
+        dbus.service.Object.__init__(self, bus, self.path)
+
+    def get_properties(self):
+        return {
+                GATT_CHRC_IFACE: {
+                        'Service': self.service.get_path(),
+                        'UUID': self.uuid,
+                        'Flags': self.flags,
+                        'Descriptors': dbus.Array(
+                                self.get_descriptor_paths(),
+                                signature='o')
+                }
+        }
+
+    def get_path(self):
+        return dbus.ObjectPath(self.path)
+
+    def add_descriptor(self, descriptor):
+        self.descriptors.append(descriptor)
+
+    def get_descriptor_paths(self):
+        result = []
+        for desc in self.descriptors:
+            result.append(desc.get_path())
+        return result
+
+    def get_descriptors(self):
+        return self.descriptors
+
+    @dbus.service.method(
+        DBUS_PROP_IFACE, in_signature='s', out_signature='a{sv}')
+    def GetAll(self, interface):
+        if interface != GATT_CHRC_IFACE:
+            raise ValueError()
+        return self.get_properties()[GATT_CHRC_IFACE]
+
+    @dbus.service.method(
+        GATT_CHRC_IFACE, in_signature='a{sv}', out_signature='ay')
+    def ReadValue(self, options):
+        raise NotImplemented()
+
+    @dbus.service.method(GATT_CHRC_IFACE, in_signature='aya{sv}')
+    def WriteValue(self, value, options):
+        raise NotImplemented()
+
+    @dbus.service.method(GATT_CHRC_IFACE)
+    def StartNotify(self):
+        raise NotImplemented()
+
+    @dbus.service.method(GATT_CHRC_IFACE)
+    def StopNotify(self):
+        raise NotImplemented()
+
+    @dbus.service.signal(DBUS_PROP_IFACE, signature='sa{sv}as')
+    def PropertiesChanged(self, interface, changed, invalidated):
+        pass
---
+++ ble_uart.py
@@ -0,0 +1,92 @@
+import sys
+import dbus
+import dbus.service
+from gi.repository import GLib
+from ble_advertisement import Advertisement
+from ble_server import Application, Service, Characteristic
+
+BLUEZ_SERVICE_NAME = 'org.bluez'
+DBUS_OM_IFACE = 'org.freedesktop.DBus.ObjectManager'
+LE_ADVERTISING_MANAGER_IFACE = 'org.bluez.LEAdvertisingManager1'
+GATT_MANAGER_IFACE = 'org.bluez.GattManager1'
+GATT_CHRC_IFACE = 'org.bluez.GattCharacteristic1'
+UART_SERVICE_UUID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e'
+UART_RX_CHARACTERISTIC_UUID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e'
+UART_TX_CHARACTERISTIC_UUID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
+LOCAL_NAME = 'usb-ble-bridge'
+
+class TxCharacteristic(Characteristic):
+
+    def __init__(self, bus, index, service):
+        super().__init__(
+            bus, index, UART_TX_CHARACTERISTIC_UUID, ['notify'], service)
+        self.notifying = False
+        GLib.io_add_watch(sys.stdin, GLib.IO_IN, self.on_console_input)
+
+    def on_console_input(self, fd, condition):
+        s = fd.readline()
+        if not s.isspace():
+            self.send_tx(s)
+        return True
+
+    def send_tx(self, s):
+        if not self.notifying:
+            return
+        value = []
+        for c in s:
+            value.append(dbus.Byte(c.encode()))
+        self.PropertiesChanged(GATT_CHRC_IFACE, {'Value': value}, [])
+
+    def StartNotify(self):
+        if not self.notifying:
+            self.notifying = True
+
+    def StopNotify(self):
+        if self.notifying:
+            self.notifying = False
+
+
+class RxCharacteristic(Characteristic):
+
+    def __init__(self, bus, index, service):
+        super().__init__(
+            bus, index, UART_RX_CHARACTERISTIC_UUID, ['write'], service)
+
+    def WriteValue(self, value, options):
+        print('remote: {}'.format(bytearray(value).decode()))
+
+
+class UartService(Service):
+
+    def __init__(self, bus, index):
+        super().__init__(bus, index, UART_SERVICE_UUID, True)
+        self.add_characteristic(TxCharacteristic(bus, 0, self))
+        self.add_characteristic(RxCharacteristic(bus, 1, self))
+
+
+class UartApplication(Application):
+
+    def __init__(self, bus):
+        super().__init__(bus)
+        self.add_service(UartService(bus, 0))
+
+
+class UartAdvertisement(Advertisement):
+
+    def __init__(self, bus, index):
+        super().__init__(bus, index, 'peripheral')
+        self.add_service_uuid(UART_SERVICE_UUID)
+        self.add_local_name(LOCAL_NAME)
+        self.include_tx_power = True
+
+
+def find_adapter(bus):
+    remote_om = dbus.Interface(bus.get_object(BLUEZ_SERVICE_NAME, '/'),
+                               DBUS_OM_IFACE)
+    objects = remote_om.GetManagedObjects()
+    for o, props in objects.items():
+        if LE_ADVERTISING_MANAGER_IFACE in props and GATT_MANAGER_IFACE in props:
+            print('Using adapter:', o)
+            return o
+        print('Skipping adapter:', o)
+    return None
