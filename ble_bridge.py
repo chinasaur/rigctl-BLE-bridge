5df6d8a2c3586ad1a7047fdebd64f3c7a123b6f6
Peter K6PLI <k6pli@arrl.net>
2025-05-16 01:09:04 -0700

    Keep rigctl open in interactive mode.
    Reported that some radios like ICOM don't like to have rigctl called
    over and over, so try connecting in interactive mode to keep a single
    rigctl process open as long as possible.

--- ble_bridge.py
+++ ble_bridge.py
@@ -1,8 +1,9 @@
 import subprocess
 import dbus
 import serial.tools.list_ports
-from ble_advertisement import Advertisement
-from ble_server import Application, Service, Characteristic
+import ble_advertisement
+import ble_server
+import rigctl
 
 BLUEZ_SERVICE_NAME = "org.bluez"
 DBUS_OM_IFACE = "org.freedesktop.DBus.ObjectManager"
@@ -12,7 +13,7 @@ GATT_CHRC_IFACE = "org.bluez.GattCharacteristic1"
 LOCAL_NAME = "rigctl-BLE-bridge"
 
 
-class TxCharacteristic(Characteristic):
+class TxCharacteristic(ble_server.Characteristic):
     UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"  # Nordic UART.
 
     def __init__(self, bus, path, service):
@@ -32,7 +33,7 @@ class TxCharacteristic(Characteristic):
         self.notifying = False
 
 
-class RxCharacteristic(Characteristic):
+class RxCharacteristic(ble_server.Characteristic):
     UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
 
     def __init__(self, bus, path, service):
@@ -44,23 +45,23 @@ class RxCharacteristic(Characteristic):
         self.service.bridge_command(cmd_str)
 
 
-class PortListCharacteristic(Characteristic):
+class PortListCharacteristic(ble_server.Characteristic):
     UUID = "d144ae91-eb03-426d-9c59-6659aa3bc324"
 
-    def __init__(self, bus, path, service, extra_ports):
-        self.extra_ports = extra_ports
+    def __init__(self, bus, path, service, extra_serial_ports):
+        self.extra_serial_ports = extra_serial_ports
         super().__init__(bus, path, self.UUID, ["read"], service)
 
-    def read_ports(self) -> list[serial.tools.list_ports_common.ListPortInfo]:
-        return serial.tools.list_ports.comports() + self.extra_ports
+    def read_serial_ports(self) -> list[serial.tools.list_ports_common.ListPortInfo]:
+        return serial.tools.list_ports.comports() + self.extra_serial_ports
 
     def ReadValue(self, options):
-        ports = self.read_ports()
-        value = ";".join(f"{p.device}:{p.description}" for p in ports)
+        serial_ports = self.read_serial_ports()
+        value = ";".join(f"{p.device}:{p.description}" for p in serial_ports)
         return [dbus.Byte(c.encode()) for c in value]
 
 
-class PortSelectCharacteristic(Characteristic):
+class PortSelectCharacteristic(ble_server.Characteristic):
     UUID = "d144ae92-eb03-426d-9c59-6659aa3bc324"
 
     def __init__(self, bus, path, service):
@@ -73,7 +74,7 @@ class PortSelectCharacteristic(Characteristic):
         self.value = value
 
 
-class HamlibDeviceCharacteristic(Characteristic):
+class HamlibDeviceCharacteristic(ble_server.Characteristic):
     UUID = "1a274176-8531-49c2-b127-a1b4138501fa"
     UNKNOWN_DEVICE = 0
 
@@ -89,11 +90,11 @@ class HamlibDeviceCharacteristic(Characteristic):
         self.value = None
 
     def map_device(self):
-        port_info = self.service.get_selected_port()
-        if port_info is None:
+        serial_port_info = self.service.get_selected_serial_port()
+        if serial_port_info is None:
             return self.UNKNOWN_DEVICE
         return self.SERIAL_PORT_DESCRIPTION_TO_HAMLIB_DEVICE_MAP.get(
-            port_info.description, self.UNKNOWN_DEVICE)
+            serial_port_info.description, self.UNKNOWN_DEVICE)
 
     def get_device(self):
         if self.value is None:
@@ -110,35 +111,42 @@ class HamlibDeviceCharacteristic(Characteristic):
         self.value = value
 
 
-class BridgeService(Service):
+class BridgeService(ble_server.Service):
     UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"  # Nordic UART
 
-    def __init__(self, bus, extra_ports):
+    def __init__(self, bus, extra_serial_ports):
         path = "/org/bluez/rigctl_bridge"
         self.tx = TxCharacteristic(bus, path + "/tx", self)
         self.rx = RxCharacteristic(bus, path + "/rx", self)
-        self.portlist = PortListCharacteristic(bus, path + "/portlist", self, extra_ports)
-        self.portselect = PortSelectCharacteristic(bus, path + "portselect", self)
+        self.serialportlist = PortListCharacteristic(bus, path + "/serialportlist", self, extra_serial_ports)
+        self.serialportselect = PortSelectCharacteristic(bus, path + "serialportselect", self)
         self.hamlib = HamlibDeviceCharacteristic(bus, path + "/hamlib", self)
-        characteristics = [self.tx, self.rx, self.portlist, self.portselect, self.hamlib]
+        characteristics = [self.tx, self.rx, self.serialportlist, self.serialportselect, self.hamlib]
         super().__init__(bus, path, self.UUID, True, characteristics)
 
-    def get_selected_port(self):
-        ports = self.portlist.read_ports()
+        self.rigctl_manager = rigctl.RigctlManager()
 
-        if not self.portselect.value:
-            if len(ports) == 1:
-                return ports[0]
+    def cull_invalid_connections(self, valid_serial_ports):
+        valid_serial_device_paths = frozenset(p.device for p in valid_serial_ports)
+        self.rigctl_manager.cleanup(valid_serial_device_paths)
+
+    def get_selected_serial_port(self):
+        serial_ports = self.serialportlist.read_serial_ports()
+        self.cull_invalid_connections(serial_ports)  # Opportunistic since we have serial ports in hand.
+
+        if not self.serialportselect.value:
+            if len(serial_ports) == 1:
+                return serial_ports[0]
             return None
 
-        for port in ports:
-            if port.device == self.portselect.value:
-                return port
-        return None  # Selected port is no longer present.
+        for serial_port in serial_ports:
+            if serial_port.device == self.serialportselect.value:
+                return serial_port
+        return None  # Selected serial port is no longer present.
 
     def bridge_command(self, cmd_str):
-        port = self.get_selected_port()
-        if port is None:
+        serial_port = self.get_selected_serial_port()
+        if serial_port is None:
             self.tx.send_tx("No serial port found for command.")
             return
 
@@ -147,30 +155,30 @@ class BridgeService(Service):
             self.tx.send_tx("No recognized Hamlib device.")
             return
 
-        cmd = ["/usr/bin/rigctl"]
-        cmd += ["-r", port.device]
-        cmd += ["-m", str(hamlib_device)]
-        cmd += cmd_str.split()
-        print(cmd)
-        cp = subprocess.run(cmd, capture_output=True, timeout=10, text=True)
-        reply = cp.stdout.strip()
-        error = cp.stderr.strip()
-        print("rigctl stdout:", reply)
-        print("rigctl stderr:", error)
+        rigctl = self.rigctl_manager.get_rigctl(serial_port.device, hamlib_device)
+        written = rigctl.write(cmd_str)
+        if not written:
+            self.tx.send_tx("Bridging to rigctl failed to write any bytes.")
+            return
 
+        reply, error = rigctl.waitread(timeout_secs=2.0)
         if error:
             self.tx.send_tx(error)
         elif reply:
             self.tx.send_tx(reply)
+        elif reply is not None:
+            self.tx.send_tx("Command accepted.")
+        else:
+            self.tx.send_tx("No reply from rigctl.")
 
 
-class BridgeApplication(Application):
-    def __init__(self, bus, extra_ports):
-        uart_service = BridgeService(bus, extra_ports)
+class BridgeApplication(ble_server.Application):
+    def __init__(self, bus, extra_serial_ports):
+        uart_service = BridgeService(bus, extra_serial_ports)
         super().__init__(bus, services=[uart_service])
 
 
-class BridgeAdvertisement(Advertisement):
+class BridgeAdvertisement(ble_advertisement.Advertisement):
     def __init__(self, bus, index):
         super().__init__(bus, index, "peripheral")
         self.add_service_uuid(BridgeService.UUID)
---
+++ rigctl.py
@@ -0,0 +1,172 @@
+import select
+import subprocess
+import time
+
+
+def connect(serial_device_path: str, hamlib_device: int) -> subprocess.Popen:
+    cmd = ["/usr/bin/rigctl", "-r", serial_device_path, "-m", str(hamlib_device)]
+    print('Starting subprocess:', cmd)
+    return subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+class RigctlSubprocess:
+    USER_PROMPT = "Rig command: "  # Filter this superfluous message out of replies.
+    TERMINATE_WAIT_SECS = 2.0
+
+    def __init__(self, serial_device_path: str, hamlib_device: int):
+        self.serial_device_path = serial_device_path
+        self.hamlib_device = hamlib_device
+        self.subprocess = connect(serial_device_path, hamlib_device)
+
+    def terminate_and_wait(self):
+        """Attempt to terminate subprocess.
+
+        Since only one process can connect to a given serial port at once, it is worth being
+        a bit slow here to try to ensure the process is dead.
+        """
+        if self.subprocess is None:
+            return
+
+        print("Terminating existing rigctl subprocess:", serial_device_path)
+        self.subprocess.terminate()
+        try:
+            self.subprocess.wait(timeout=self.TERMINATE_WAIT_SECS)
+        except subprocess.TimeoutExpired as e:
+            print("terminate timeout:", e)
+
+        self.subprocess.kill()
+        try:
+            self.subprocess.wait(timeout=self.TERMINATE_WAIT_SECS)
+        except subprocess.TimeoutExpired as e:
+            print("kill timeout:", e)
+
+    def reconnect(self):
+        self.terminate_and_wait()
+        self.subprocess = connect(self.serial_device_path, self.hamlib_device)
+        return None
+
+    def select(self):
+        """Query OS for which pipes are ready / errored."""
+        readable = [self.subprocess.stdout, self.subprocess.stderr]
+        writable = [self.subprocess.stdin]
+        errored = [self.subprocess.stdout, self.subprocess.stderr, self.subprocess.stdin]
+        return select.select(readable, writable, errored)
+
+    def poll(self):
+        retval = self.subprocess.poll()
+        if retval is not None:
+            print(f"rigctl on {self.serial_device_path} found to be dead with retval {retval}")
+        return retval
+
+    def write(self, cmd_str: str) -> int | None:
+        # If we detect that subprocess has gone bad, try to restart it, but give up on writing for now.
+        _, writable, errored = self.select()
+        if errored:
+            print("Error on rigctl pipes.")
+            return self.reconnect()
+        if not writable:
+            print("rigctl stdin not writable.")
+            return self.reconnect()
+        assert len(writable) == 1
+
+        cmd_str = cmd_str.strip() + "\n"  # rigctl is better behaved this way.
+        try:
+            written = writable[0].write(cmd_str.encode())
+            writable[0].flush()
+        except BrokenPipeError as e:
+            print(e)
+            return self.reconnect()
+        return written
+
+    def safe_read(self, pipe) -> str | None:
+        """Try to ensure we avoid hanging while reading pipes."""
+        readable, _, _ = self.select()
+        if pipe not in readable:
+            return None
+        l = len(pipe.peek())
+        return pipe.read(l).decode()
+
+    def parse(self, out: str) -> str:
+        """Clean up output."""
+        lines = out.replace(self.USER_PROMPT, "").split("\n")
+        return "; ".join(l for l in lines if l)  # Drop any empty ones.
+
+    def read(self) -> tuple[str | None, str | None]:
+        _, _, errored = self.select()
+        if errored:
+            print("Error on rigctl pipes.")
+            self.reconnect()
+            return None, None
+
+        out = self.safe_read(self.subprocess.stdout)
+        if out is not None and not out:
+            # An empty reply is valid for set commands, but it can also indicate the process died.
+            if self.poll() is not None:
+                self.reconnect()
+        if out is not None:
+            out = self.parse(out)
+
+        err = self.safe_read(self.subprocess.stderr)
+        if err:
+            print(err)
+
+        return out, err
+
+    def waitread(self, timeout_secs: float) -> tuple[str | None, str | None]:
+        start = time.time()
+        out, err = None, None
+        if timeout_secs <= 0.0:
+            return None, None
+
+        readable, _, errored = self.select()
+        if readable:
+            out, err = self.read()
+        if errored:
+            print("Error on rigctl pipes.")
+            self.reconnect()
+        if readable:
+            return out, err
+
+        time.sleep(0.1)
+        elapsed = time.time() - start
+        return self.waitread(timeout_secs - elapsed)
+
+
+class RigctlManager:
+
+    def __init__(self):
+        # Only one process can connect to a given serial device path at a time.
+        self.rigctl_from_serial_device = {}
+
+    def maybe_terminate_existing(self, serial_device_path: str):
+        existing_rigctl = self.rigctl_from_serial_device.get(serial_device_path)
+        if existing_rigctl is None:
+            return
+        if existing_rigctl.poll() is not None:
+            return  # Already dead.
+        existing_rigctl.terminate_and_wait()
+
+    def refresh(self, serial_device_path: str, hamlib_device: int):
+        self.maybe_terminate_existing(serial_device_path)
+        self.rigctl_from_serial_device[serial_device_path] = RigctlSubprocess(serial_device_path, hamlib_device)
+
+    def get_rigctl(self, serial_device_path: str, hamlib_device: int) -> RigctlSubprocess:
+        if serial_device_path not in self.rigctl_from_serial_device:
+            self.refresh(serial_device_path, hamlib_device)
+        if self.rigctl_from_serial_device[serial_device_path].hamlib_device != hamlib_device:
+            # The Hamlib device ID for this serial port has changed, so we need to get a new
+            # RigctlSubprocess.
+            self.refresh(serial_device_path, hamlib_device)
+        if self.rigctl_from_serial_device[serial_device_path].poll() is not None:
+            # Subprocess died for some reason; try restarting.
+            self.refresh(serial_device_path, hamlib_device)
+        return self.rigctl_from_serial_device[serial_device_path]
+
+    def cleanup(self, valid_serial_device_paths: frozenset[str]):
+        for serial_device_path, rigctl in self.rigctl_from_serial_device.items():
+            if serial_device_path in valid_serial_device_paths:
+                continue
+            # Serial port no longer detected, so clear out any rigctl that was previously
+            # connected to it. We don't want to wait for this though.
+            rigctl.subprocess.terminate()
+            self.rigctl_from_serial_device.pop(serial_device_path, None)
